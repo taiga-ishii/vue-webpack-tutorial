5998ae7896dd73bea056b18d8a22c5707d41ae0b
Taiga Ishii <taigaishii@TaiganoMacBook-Pro.local>
2017-12-31 11:04:24 +0900

    helloFromExpress

--- src/server/index.js
+++ src/server/index.js
@@ -1,2 +1,7 @@
-import hello from './hello'
-console.log(hello())
+import express from 'express'
+import renderApp from './renderApp'
+const app =express()
+app.get('/', (req, res)=>{
+  res.send(renderApp())
+})
+app.listen(3000, ()=> console.log('Listening on port 3000.'))
---
+++ src/server/renderApp.js
@@ -0,0 +1,15 @@
+const renderApp= () =>
+`
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <div id="app"></div>
+<body>
+  <h3>Hello from express</h3>
+  <div id="app"></div>
+</body>
+</html>
+`
+
+export default renderApp
